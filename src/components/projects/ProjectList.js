afdb16ec228867571206396cf7f4a1ae678a756f
Liam Dawson <liamrdawson@gmail.com>
2020-01-05 18:07:07 +0000

    replace ProjectList items with ProjectSummary components

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Liam</p>
-                    <p className="grey-text">5th January, 17:50</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Liam</p>
-                    <p className="grey-text">5th January, 17:50</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Liam</p>
-                    <p className="grey-text">5th January, 17:50</p>
-                </div>
-            </div>
-
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
         </div>
     )
 }
