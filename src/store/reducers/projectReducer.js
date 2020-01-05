c60e4001d3327c1b36a95f4a9ddb3d3e740bfd43
Liam Dawson <liamrdawson@gmail.com>
2020-01-05 22:18:19 +0000

    add placeholder data to projectReducer and render to dashboard

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+    projects: [
+        {id: 1, title: 'Learn Redux', content: 'Learn more about Redux by building a project planner app.'},
+        {id: 2, title: 'Thunk', content: 'Add Thunk to your project.'},
+        {id: 3, title: 'Firebase', content: 'Set up a backend using Firebase.'}
+    ]
+};
 
 const projectReducer = (state = initState, action) => {
     return state;
